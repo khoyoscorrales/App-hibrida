62be5332a4091401b7baf1a3b71969d662e58754
laura-152020 <104606089+laura-152020@users.noreply.github.com>
2024-05-12 18:19:47 -0500

    finished note add

--- app/src/main/java/com/example/quicknotes/MainActivity.java
+++ app/src/main/java/com/example/quicknotes/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.quicknotes;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
         addNoteBtn = findViewById(R.id.add_note_btn);
 
-        addNoteBtn.setOnClickListener((v)->);
+        addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,NoteDetailsActivity.class)) );
     }
 }
--- app/src/main/java/com/example/quicknotes/NoteDetailsActivity.java
+++ app/src/main/java/com/example/quicknotes/NoteDetailsActivity.java
@@ -1,6 +1,8 @@
 package com.example.quicknotes;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class NoteDetailsActivity extends AppCompatActivity {
 
+    EditText titleEditText,contentEditText;
+    ImageButton saveNoteBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_note_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        saveNoteBtn = findViewById(R.id.save_note_btn);
+
+        saveNoteBtn.setOnClickListener((v-> saveNote()));
+    }
+
+    void saveNote(){
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent= contentEditText.getText().toString();
+        if(noteTitle==null || noteTitle.isEmpty()){
+            titleEditText.setError("Title is required");
+            return;
+        }
     }
 }
