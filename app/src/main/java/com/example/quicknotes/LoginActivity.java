3a3d831712571ab9a17fc4902d1fce6a80048400
laura-152020 <104606089+laura-152020@users.noreply.github.com>
2024-05-11 22:52:55 -0500

    diseño log in done

--- app/src/main/java/com/example/quicknotes/LoginActivity.java
+++ app/src/main/java/com/example/quicknotes/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.quicknotes;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
