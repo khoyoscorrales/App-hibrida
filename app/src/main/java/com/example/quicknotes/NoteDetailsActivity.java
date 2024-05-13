60e04661453fee3d0584f32ba9c514d2b5dfb02d
laura-152020 <104606089+laura-152020@users.noreply.github.com>
2024-05-12 19:48:51 -0500

    java class note

--- app/src/main/java/com/example/quicknotes/NoteDetailsActivity.java
+++ app/src/main/java/com/example/quicknotes/NoteDetailsActivity.java
@@ -1,14 +1,21 @@
 package com.example.quicknotes;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 
 public class NoteDetailsActivity extends AppCompatActivity {
 
@@ -34,5 +41,30 @@ public class NoteDetailsActivity extends AppCompatActivity {
             titleEditText.setError("Title is required");
             return;
         }
+        Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setContent(noteContent);
+        note.setTimestamp(Timestamp.now());
+
+        saveNoteToFirebase(note);
+    }
+
+    void saveNoteToFirebase(Note note){
+        DocumentReference documentReference;
+        documentReference = utility.getCollectionReferenceForNotes().document();
+
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    //note is added
+                    utility.showToast(NoteDetailsActivity.this, "Note added successfully");
+                    finish();
+                }else{
+                    utility.showToast(NoteDetailsActivity.this, "Failed while adding note");
+
+                }
+            }
+        });
     }
 }
